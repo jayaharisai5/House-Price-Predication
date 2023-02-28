168cb2161ead8fa929cb9c119b598c22a371945a
jayaharisai5 <100357364+jayaharisai5@users.noreply.github.com>
2023-02-28 15:08:09 +0530

    Update _1_load_data.py

--- _1_load_data.py
+++ _1_load_data.py
@@ -5,5 +5,6 @@ import pandas as pd
 def load_data():
     train_df = pd.read_json("Train.json")
     test_df = pd.read_json("Test.json")
+    print("Hello world")
     test_target = pd.read_json("sample_submission.json")
     return(train_df, test_df, test_target)
